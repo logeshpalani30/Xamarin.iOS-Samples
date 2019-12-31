03b92880dbe141cf73fb8b9a99f9e88ddcdda022
logesh <logesh@hakunamatata.in>
2019-12-31 11:03:27 +0530

    OTP View

---
+++ OTPSample/OTPSample/ViewController.cs
@@ -0,0 +1,75 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace OTPSample
+{
+    public partial class ViewController : UIViewController, IUITextFieldDelegate
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            fieldOne.Delegate = this;
+            fieldTwo.Delegate = this;
+            fieldThree.Delegate = this;
+            fieldFour.Delegate = this;
+
+            fieldOne.BackgroundColor = UIColor.Clear;
+            fieldTwo.BackgroundColor = UIColor.Clear;
+            fieldThree.BackgroundColor = UIColor.Clear;
+            fieldFour.BackgroundColor = UIColor.Clear;
+
+        }
+
+        [Export("textField:shouldChangeCharactersInRange:replacementString:")]
+        public bool ShouldChangeCharacters(UITextField textField, NSRange range, string replacementString)
+        {
+            if (textField.Text?.Length < 1 && replacementString.Length > 0)
+            {
+                if (textField == fieldOne)
+                    fieldTwo.BecomeFirstResponder();
+                else if (textField == fieldTwo)
+                    fieldThree.BecomeFirstResponder();
+                else if (textField == fieldThree)
+                    fieldFour.BecomeFirstResponder();
+                else if (textField == fieldFour)
+                    fieldFour.ResignFirstResponder();
+
+                textField.Text = replacementString;
+                return false;
+            }
+            else if (textField.Text?.Length >= 1 && replacementString.Length == 0)
+            {
+                if (textField == fieldTwo)
+                    fieldOne.BecomeFirstResponder();
+                else if (textField == fieldThree)
+                    fieldTwo.BecomeFirstResponder();
+                else if (textField == fieldFour)
+                    fieldThree.BecomeFirstResponder();
+                else if (textField == fieldOne)
+                    fieldOne.BecomeFirstResponder();
+
+                textField.Text = "";
+                return false;
+            }
+            else if (textField.Text?.Length >= 1)
+            {
+                textField.Text = replacementString;
+                return false;
+            }
+            return true;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ OTPSample/OTPSample/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace OTPSample
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField fieldFour { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField fieldOne { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField fieldThree { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField fieldTwo { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (fieldFour != null) {
+                fieldFour.Dispose ();
+                fieldFour = null;
+            }
+
+            if (fieldOne != null) {
+                fieldOne.Dispose ();
+                fieldOne = null;
+            }
+
+            if (fieldThree != null) {
+                fieldThree.Dispose ();
+                fieldThree = null;
+            }
+
+            if (fieldTwo != null) {
+                fieldTwo.Dispose ();
+                fieldTwo = null;
+            }
+        }
+    }
+}
